fb9c2396c3bf7b7de0585a05dc061f07ef3f97df
gilang_gans <gilangsos99@gmail.com>
2022-05-16 18:30:05 +0700

    Tugas 22 js
    Tugas ini untuk niomic

---
+++ javascript/tugas22.js
@@ -0,0 +1,8 @@
+function panggilFOrEAch(){
+ var data = ["saya ","ingin", "belajar", "bersama"];
+ data.forEach(function(item, index){
+   console.log("item :", item ,"index ke", index )
+ })
+}
+
+panggilFOrEAch();
